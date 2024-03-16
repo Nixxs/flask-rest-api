c271b01df2c1af8afac52fcd1a4a7fb684788560
Nicholas Chai <nicholas.chai@outlook.com>
2024-03-16 15:10:28 +0800

    added update and delete routes

--- app.py
+++ app.py
@@ -47,6 +47,32 @@ def create_store():
 
     return store, 201
 
+@app.delete("/store/<string:store_id>")
+def delete_item(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}, 201
+    except KeyError:
+        abort(404, message="store not found")
+
+@app.put("/stores/<string:store_id>")
+def update_item(store_id):
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad Request, please include name in payload")
+    
+    # if store name already exists reject
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
+    try:
+        store = stores[store_id]
+        store |= store_data
+        return store
+    except KeyError:
+        abort(404, message="store not found")
+
 @app.get("/item")
 def get_all_items():
     return {"items": list(items.values())}
@@ -58,7 +84,6 @@ def get_item(item_id):
     except KeyError:
         abort(404, message="Item not found.")
     
-
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
@@ -87,5 +112,23 @@ def create_item():
 
     return item
 
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}, 201
+    except KeyError:
+        abort(404, message="item not found")
 
-    
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad Request, please include price and name in payload")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        abort(404, message="item not found")
