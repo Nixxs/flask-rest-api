f342b9393c1d62dc4747defe8789f2c6f38a3510
Nicholas Chai <nicholas.chai@outlook.com>
2024-03-17 14:06:33 +0800

    Decorate routes with schema validation for dat agoing out as well

--- resources/store.py
+++ resources/store.py
@@ -9,10 +9,12 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route("/store")
 class Stores(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
     
     @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
     def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
@@ -22,10 +24,11 @@ class Stores(MethodView):
         store = {**store_data, "id": store_id}
         stores[store_id] = store
 
-        return store, 201
+        return store
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -33,6 +36,7 @@ class Store(MethodView):
             abort(404, message="Store not found.")
     
     @blp.arguments(UpdateStoreSchema)
+    @blp.response(200, StoreSchema)
     def put(self, store_data, store_id):
         if "name" not in store_data:
             abort(400, message="Bad Request, please include name in payload")
